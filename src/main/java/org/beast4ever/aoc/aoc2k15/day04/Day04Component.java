61c1255ea167f0c25479d2abc7a969b13ac040f7
david.bernier <david.bernier@soprasteria.com>
2023-11-17 18:01:06 +0100

    Day 04 - Completed

---
+++ src/main/java/org/beast4ever/aoc/aoc2k15/day04/Day04Component.java
@@ -0,0 +1,81 @@
+package org.beast4ever.aoc.aoc2k15.day04;
+
+import lombok.extern.slf4j.Slf4j;
+import org.beast4ever.aoc.aoc2k15.DayResolutionComponent;
+import org.beast4ever.aoc.aoc2k15.FileUtilityParser;
+import org.beast4ever.aoc.aoc2k15.day03.Day03Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+@Component
+@Slf4j
+public class Day04Component extends DayResolutionComponent {
+    @Autowired
+    private FileUtilityParser fileUtilityParser;
+
+    @Override
+    public String resolveFirstStar(String inputFilePath) throws IOException {
+        String secret = fileUtilityParser.readFirstLine(inputFilePath);
+        return "" + findLowestPositiveNumber(secret, 5);
+    }
+
+    @Override
+    public String resolveSecondStar(String inputFilePath) throws IOException {
+        String secret = fileUtilityParser.readFirstLine(inputFilePath);
+        return "" + findLowestPositiveNumber(secret, 6);
+    }
+
+    private Long findLowestPositiveNumber(String secret, Integer nbOfLeadingZeros) {
+        Long lowestPositiveNumber = 0l;
+        StringBuilder leadingZeroStr = new StringBuilder();
+
+        IntStream.range(0, nbOfLeadingZeros).forEach( i -> leadingZeroStr.append("0"));
+
+        for (Long l=0l; l<=Long.MAX_VALUE; l++) {
+            String phraseToHash = secret + l;
+            String md5Hash = getMd5(phraseToHash);
+            if (md5Hash.startsWith(leadingZeroStr.toString())) {
+                lowestPositiveNumber = l;
+                break;
+            }
+        }
+
+        return lowestPositiveNumber;
+    }
+
+    private String getMd5(String input)
+    {
+        try {
+
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // digest() method is called to calculate message digest
+            // of an input digest() return array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/org/beast4ever/aoc/aoc2k15/day04/Day04ComponentTest.java
@@ -0,0 +1,51 @@
+package org.beast4ever.aoc.aoc2k15.day04;
+
+import org.beast4ever.aoc.aoc2k15.day03.Day03Component;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+public class Day04ComponentTest {
+    @Autowired
+    Day04Component day04Component;
+
+    @Test
+    public void whenInputTestData01_thenResolveFirstStar() throws IOException {
+        String expectedResponse = "609043";
+
+        String response = day04Component.resolveFirstStar("test-input-day04-01.txt");
+        Assertions.assertEquals(expectedResponse, response);
+
+    }
+
+    @Test
+    public void whenInputTestData02_thenResolveFirstStar() throws IOException {
+        String expectedResponse = "1048970";
+
+        String response = day04Component.resolveFirstStar("test-input-day04-02.txt");
+        Assertions.assertEquals(expectedResponse, response);
+
+    }
+
+    @Test
+    public void whenInputRealData_thenResolveFirstStar() throws IOException {
+        String expectedResponse = "346386";
+
+        String response = day04Component.resolveFirstStar("input-day04-S1S2.txt");
+        Assertions.assertEquals(expectedResponse, response);
+
+    }
+
+    @Test
+    public void whenInputRealData_thenResolveSecondStar() throws IOException {
+        String expectedResponse = "9958218";
+
+        String response = day04Component.resolveSecondStar("input-day04-S1S2.txt");
+        Assertions.assertEquals(expectedResponse, response);
+
+    }
+}
